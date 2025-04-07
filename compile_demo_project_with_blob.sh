89fd790cf29575f63d0073730607d7c287dcded8
yymin1022 <yymin1022@gmail.com>
2025-04-07 07:13:04 +0000

    Added compile_demo_project_with_blob.sh : Script for compile demo with blob data file weights_blob.bin

---
+++ compile_demo_project_with_blob.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+echo ##########
+echo "Compiling Demo..."
+echo ##########
+
+gcc -o rnnoise_demo \
+	examples/rnnoise_demo.c \
+	src/celt_lpc.c src/denoise.c src/kiss_fft.c \
+	src/nnet.c src/nnet_default.c src/parse_lpcnet_weights.c \
+	src/pitch.c src/rnn.c src/rnnoise_tables.c \
+	-I./include -I./src \
+	-lm \
+	-DUSE_WEIGHTS_FILE
+
+echo ##########
+echo "Compiled rnnoise_demo !!"
+ls -alh | grep rnnoise_demo
+echo ##########
